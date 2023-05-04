fc395d160092a4c6992e32668142f0764bafe891
SasakiHayato0605 <zuomusundou685@gmail.com>
2023-05-05 01:45:13 +0900

    ゲームデータクラスの仮置き

---
+++ Assets/Scripts/Data/GameLocalData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLocalData : SingletonBehaviour<GameLocalData>
+{
+    protected override void Initialize()
+    {
+        
+    }
+}
